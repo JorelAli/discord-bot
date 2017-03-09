0064788eb1c470d2dc97130762247e8faa2dbccd
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-03-09 19:24:24 +0000

    Adjusts inputs: "Cynthia" is now a command

--- inputs.js
+++ inputs.js
@@ -1,7 +1,8 @@
 module.exports = {
-    greetings: ['😄', '😃', 'hi', 'hello', 'hey', ':)', ':D', 'hai', 'cynthia', 'oi'],
+    greetings: ['😄', '😃', 'hi', 'hello', 'hey', ':)', ':D', 'hai', 'oi'],
     negativeFeelings: [':/', 'sigh', '😦', ':(', 'D:'],
     yes: ['yeah', 'yes', 'yup', 'sure', 'why not?', 'why not', 'yep', 'yea', 'ok'],
     no: ['nah', "I'm good", 'Im good', 'no', 'nope', 'not today', 'maybe later'],
-    terminator: ["that's enough", "thats enough", "cool, thanks", "cool thanks", "thanks", "bye", "see ya", "see you", "goodbye", "farewell"]
+    terminator: ["that's enough", "thats enough", "cool, thanks", "cool thanks", "thanks", "bye", "see ya", "see you", "goodbye", "farewell"],
+    command: ['cynthia', 'tell']
 }
